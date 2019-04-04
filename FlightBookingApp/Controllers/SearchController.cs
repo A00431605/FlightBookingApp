551c26b454510c86c362264fa1103506419621ca
Allianzcortex <iamwanghz@gmail.com>
2019-04-04 20:09:17 -0300

    implement the interaction between front-end and back-end

--- FlightBookingApp/Controllers/SearchController.cs
+++ FlightBookingApp/Controllers/SearchController.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FlightBookingApp.Models;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+
 
 namespace FlightBookingApp.Controllers
 {
     public class SearchController : Controller
     {
         // POST:search available flights
+        
+        /**public ActionResult Index(object form)
+        {
+            Console.WriteLine(form);
+            return View();
+        }**/
+
         [HttpPost]
+        public ActionResult Index(Search search)
+        {
+            // Console.WriteLine("search is :");
+            System.Diagnostics.Debug.WriteLine("----------------");
+            System.Diagnostics.Debug.WriteLine(search.Status);
+
+
+            return View();
+        }
+        
+        [HttpGet]
         public ActionResult Index()
         {
-            return View("SearchPage");
+            return View();
         }
     }
 }
---
+++ FlightBookingApp/Models/Search.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightBookingApp.Models
+{
+    public class Search
+    {
+        [Required]
+        [MaxLength(140)]
+        public string Status { get; set; }
+    }
+}
