9b876af3c885e5c3781629941cb797a05dc08e12
A00431796 <subha.vijayakumar@smu.ca>
2019-04-04 10:00:49 -0300

    first commit

---
+++ FlightBookingApp/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlightBookingApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult OrderCancel()
+        {
+            return View();
+        }
+    }
+}
