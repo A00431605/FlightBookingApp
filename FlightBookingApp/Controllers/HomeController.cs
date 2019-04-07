47417777c0784db21f7dc1ee881c2e592768dd78
A00433097 <46535000+A00433097@users.noreply.github.com>
2019-04-07 11:50:08 -0300

    Login/SignUp V2

--- FlightBookingApp/Controllers/HomeController.cs
+++ FlightBookingApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿
-using System;
+using FlightBookingApp.Models;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 
 namespace FlightBookingApp.Controllers
 {
@@ -14,7 +16,7 @@ namespace FlightBookingApp.Controllers
             return View();
         }
 
-       
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -24,21 +26,49 @@ namespace FlightBookingApp.Controllers
 
         public ActionResult Login()
         {
-           
+
             return View();
         }
 
         [HttpPost]
-        public ActionResult Autherize(FlightBookingApp.Models.customer_book customerModel)
+        public ActionResult Login(customer_book customerModel)
+        {
+            using (FlightBookingEntity db = new FlightBookingEntity())
+            {
+                var obj = db.customer_book.Where(x => x.customer_emailaddress == customerModel.customer_emailaddress && x.customer_password == customerModel.customer_password).FirstOrDefault();
+                if (obj == null)
+                {
+                    customerModel.LoginErrorMessage = "Please enter valid Username and Password";
+                    return View("Login",customerModel);
+                }
+                TempData["usernname"] = obj.customer_name;
+                TempData.Keep();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult signUp()
         {
             return View();
         }
 
-        public  ActionResult signUp()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult signUp(customer_book customerModel1)
         {
-            return View();
-        }
+                using (FlightBookingEntity db = new FlightBookingEntity())
+                {
+                    db.customer_book.Add(customerModel1);
+                    db.SaveChanges();
 
-        
-    }
+                }
+                ModelState.Clear();
+                ViewBag.SuccessMessage = "Registration Successful.";
+                customerModel1 = null;
+           
+         
+                return View("signUp",new customer_book());
+        }
+    } 
 }
